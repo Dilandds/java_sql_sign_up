9dd1cbab3796bee0c533d65e490a281bf49180cc
Dilandds <e17379@eng.pdn.ac.lk>
2023-10-08 01:40:29 +0600

    added sql connection

--- src/main/java/com/dilandds/registration/RegistrationServlet.java
+++ src/main/java/com/dilandds/registration/RegistrationServlet.java
@@ -1,12 +1,15 @@
 package com.dilandds.registration;
 import java.io.PrintWriter;
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.sql.*;
 /**
  * Servlet implementation class RegistrationServlet
  */
@@ -17,10 +20,47 @@ public class RegistrationServlet extends HttpServlet {
    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		PrintWriter out = response.getWriter();
-		out.print("Working");
+		String uname = request.getParameter("name");
+		String uemail = request.getParameter("email");
+		String upwd = request.getParameter("pass");
+		String umobile = request.getParameter("contact");
 		
+//		PrintWriter out = response.getWriter();
+//		out.print(uname);
+//		out.print(uemail);
+//		out.print(upwd);
+//		out.print(umobile);
 		
-	}
+		RequestDispatcher dispatcher = null;
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			 con= DriverManager.getConnection("jdbc:mysql://localhost:3306/youtube", "root", "bstindewrld");
+			PreparedStatement pst = con.prepareStatement("insert into users (uname, upwd, uemail, umobile) values (?,?,?,?) ");
+			pst.setString(1, uname);
+			pst.setString(2, upwd);
+			pst.setString(3, uemail);
+			pst.setString(4, umobile);
+			
+			int rowCount = pst.executeUpdate();
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			if (rowCount > 0) {
+			request.setAttribute("status", "success");
+			}else {
+			request.setAttribute("status", "failed");
+			}
+			dispatcher.forward(request, response);
+		}catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+		    e.printStackTrace(); // You can log this exception or handle it as needed.
 
+		}finally {
+			try {
+			con.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
+		}
+	}
+	}
 }
