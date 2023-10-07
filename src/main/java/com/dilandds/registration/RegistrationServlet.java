14b45b2c104ead1d5ac0c149b403839e44b7d65e
Dilandds <e17379@eng.pdn.ac.lk>
2023-10-08 00:32:21 +0600

    added frontend codes

---
+++ src/main/java/com/dilandds/registration/RegistrationServlet.java
@@ -0,0 +1,26 @@
+package com.dilandds.registration;
+import java.io.PrintWriter;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegistrationServlet
+ */
+@WebServlet("/register")
+public class RegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		out.print("Working");
+		
+		
+	}
+
+}
